08bbfe05213ba04cf4ae86e6fac2710adbe2e9be
xfeng82 <xfeng82@wisc.edu>
2019-12-21 15:45:34 -0600

    sqrt function

---
+++ java/sqrt/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+    public static double fuc(double a,double b){  /*a is the unknown*/
+        double fx;
+        fx=a*a-b;
+        return fx;
+    }
+    public static double dot(double a,double b){
+        double fx;
+        fx=2*a;
+        return fx;
+    }
+    public static double sqrt(double value){
+        double a=value+1;
+        double flag;
+        while(fuc(a,value)>=1e-15){
+            double dot=dot(a,value);
+            a=((-fuc(a,value))/dot)+a;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        double value=5;
+        double root=sqrt(value);
+        System.out.println(root);
+    }
+}
