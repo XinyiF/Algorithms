593cc341964257b8f4ace5d72c18b3594a2c804c
xfeng82 <xfeng82@wisc.edu>
2019-12-25 17:00:10 -0600

    fix bugs

--- java/ShellSort/src/com/company/Main.java
+++ java/ShellSort/src/com/company/Main.java
@@ -6,13 +6,14 @@ public class Main {
         int step=len/2;
         while(step>0){
             for(int i=0;i<step;i++){
-                for(int j=i+step;j<len;j+=step){
+                for(int j=len-1;j>0;j-=step){
                     int temp;
                     for(int k=j-step;k>=0;k-=step){
                         if(nums[k]>nums[j]){
                             temp=nums[k];
                             nums[k]=nums[j];
                             nums[j]=temp;
+                            break;
                         }
                     }
                 }
@@ -22,7 +23,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int [] nums={2,4,5,3,32,33,12,32,0,1,2,3};
+        int [] nums={5,2,6,1};//5162 5126 2156
         ShellSort(nums);
         for(int i=0;i<nums.length;i++){
             System.out.println(nums[i]);
