ca9e7a170e25449aaa47f072b4c98b2e083539b2
XinyiF <56609063+xinyif@users.noreply.github.com>
2019-12-25 13:15:42 -0600

    find duplicate number in an array, simply nums[i]==nums[i+1]

---
+++ java/findDuplicate_287/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+//Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive),
+// prove that at least one duplicate number must exist.
+// Assume that there is only one duplicate number, find the duplicate one.
+
+import java.util.Arrays;
+
+public class Main {
+    public static int findDuplicate(int[] nums){
+        Arrays.sort(nums);
+        int i;
+        for(i=0;i<nums.length-2;i++){
+            if(nums[i]==nums[i+1]){
+                break;
+            }
+        }
+        return nums[i];
+    }
+
+    public static void main(String[] args) {
+	int []nums={1,3,4,2,2};
+	int res=findDuplicate(nums);
+	System.out.println(res);
+    }
+}
