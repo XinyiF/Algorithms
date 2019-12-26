f7b524bfa2a9756f8aabd91e67f26cd202b451c4
XinyiF <56609063+xinyif@users.noreply.github.com>
2019-12-25 18:45:47 -0600

    add new feature: star point only sort values after star point

--- java/ShellSort/src/com/company/Main.java
+++ java/ShellSort/src/com/company/Main.java
@@ -1,19 +1,18 @@
 package com.company;
 
 public class Main {
-    public static void ShellSort(int[] nums){
-        int len=nums.length;
+    public static void ShellSort(int[] nums,int star){
+        int len=nums.length-star;
         int step=len/2;
         while(step>0){
-            for(int i=0;i<step;i++){
-                for(int j=len-1;j>0;j-=step){
+            for(int i=star;i<star+step;i++){
+                for(int j=nums.length-1;j>star;j-=step){
                     int temp;
-                    for(int k=j-step;k>=0;k-=step){
+                    for(int k=j-step;k>=star;k-=step){
                         if(nums[k]>nums[j]){
                             temp=nums[k];
                             nums[k]=nums[j];
                             nums[j]=temp;
-                            break;
                         }
                     }
                 }
@@ -23,8 +22,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int [] nums={5,2,6,1};//5162 5126 2156
-        ShellSort(nums);
+        int [] nums={5,2,6,1,3,9,8,10,12,54,13,11};//5131269 5121369 5112369
+        ShellSort(nums,2);
         for(int i=0;i<nums.length;i++){
             System.out.println(nums[i]);
         }
