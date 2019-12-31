ff0361621aaf2482fe444c8e9016917097551c85
xfeng82 <xfeng82@wisc.edu>
2019-12-31 15:00:20 -0600

    cleaner, but slower...just learn collections

--- Python/Map/4Sum_454.py
+++ Python/Map/4Sum_454.py
@@ -2,26 +2,19 @@
 # there are such that A[i] + B[j] + C[k] + D[l] is zero.
 #To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500.
 # All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+import collections
+from collections import Counter
+
 
 class Solution(object):
     def fourSumCount(self, A, B, C, D):
-        count ={}
-        for a in A:
-            for b in B:
-                if(a+b) in count:
-                    count[a+b]+=1
-                else:
-                    count[a+b]=1
-        counter=0
-        for c in C:
-            for d in D:
-                if(-c-d in count):
-                    counter+=count[-c-d]
-        return counter
+        h = collections.Counter(-a - b for a in A for b in B)
+        return sum(h[c+d] for c in C for d in D)
 
 A=[1,2]
 B=[-2,-1]
 C=[-1,2]
 D=[0,2]
 res=Solution()
-res.fourSumCount(A,B,C,D)
+sum=res.fourSumCount(A,B,C,D)
+print(sum)
