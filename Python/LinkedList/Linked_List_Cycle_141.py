2fe5c61d7e1fd355b3d5c9e07e7b7e0cbfe3a146
xfeng82 <xfeng82@wisc.edu>
2019-12-31 16:51:48 -0600

    single linkedlist. if it has a cycle

---
+++ Python/LinkedList/Linked_List_Cycle_141.py
@@ -0,0 +1,39 @@
+#Given a linked list, determine if it has a cycle in it.
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        count={}
+        if (head == None): return False
+        while head:
+            print(head)
+            if(head not in count):
+                count[head]=1
+            else:
+                return True
+            if(head.next!=None):
+                head = head.next
+            else:
+                return False
+
+
+
+
+l0=ListNode(3)
+l1=ListNode(2)
+l2=ListNode(2)
+l3=ListNode(4)
+
+l0.next=l1
+l1.next=l2
+l2.next=l3
+l3.next=None
+
+
+res=Solution()
+r=res.hasCycle(l0)
