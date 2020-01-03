0030e27cd4d9c1bc2c725a811326380cfd8670e8
xfeng82 <xfeng82@wisc.edu>
2020-01-02 22:32:42 -0600

    make code cleaner but not faster...

--- Python/DynamicProgramming/Longest_Palindromic_Substring_5.py
+++ Python/DynamicProgramming/Longest_Palindromic_Substring_5.py
@@ -39,11 +39,34 @@ class Solution(object):
             count[cur1]=Right-Left+1
         return max(count,key=count.get)
 
+    def CenterExpand(self,left,right,cur,count,s):
+        while left>=0 and right<len(s):
+            if s[left]==s[right]:
+                cur = s[left] + cur + s[right]
+                left-=1;right+=1
+            else:break
+        left+=1;right-=1
+        count[cur]=right-left+1
 
-str="babad"
+    def longestPalindrome_new(self, s):
+        if len(s)<2 or len(s)==2 and s[0]==s[1]:return s
+        if len(s)==2 and s[0]!=s[1]:return s[0]
+        count={}
+        for i in range(len(s)-1):
+            cur1,cur2=s[i],''
+            self.CenterExpand(i-1,i+1,cur1,count,s)
+            self.CenterExpand(i,i+1,cur2,count,s)
+        return max(count,key=count.get)
+
+
+
+
+
+str="aaaabbccbb"
 res=Solution()
-s=res.longestPalindrome(str)
+s=res.longestPalindrome_new(str)
 print(s)
+
 
 
 
