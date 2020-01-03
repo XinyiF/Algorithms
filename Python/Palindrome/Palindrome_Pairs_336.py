232c141967b31dba5add789328ab5dcea9a6ba5b
xfeng82 <xfeng82@wisc.edu>
2020-01-03 17:16:42 -0600

    I find a better way

--- Python/Palindrome/Palindrome_Pairs_336.py
+++ Python/Palindrome/Palindrome_Pairs_336.py
@@ -2,7 +2,7 @@
 # so that the concatenation of the two words, i.e. words[i] + words[j] is a palindrome.
 
 class Solution(object):
-    def palindromePairs(self, words):
+    def palindromePairs_BF(self, words):
         res=[]
         for i in range(len(words)-1):
             for j in range(i+1,len(words)):
@@ -25,6 +25,39 @@ class Solution(object):
                         if temp[0:mid] == temp[mid + 1:len(words[i]) + len(words[j])][::-1]:
                             pair = [j, i];res.append(pair)
         return res
+
+
+    def palindromePairs(self, words):
+        # store words who is palindrome itself, include ''
+        palidStr = []
+        # reverse words and their index
+        rev_words = {}
+        # result
+        res = []
+        for idx, word in enumerate(words):
+            rev_words[word[::-1]] = idx
+            if word == word[::-1]:
+                palidStr.append(idx)
+        for idx, word in enumerate(words):
+            if word:
+                for i in range(len(word)):
+                    left, right = word[:i], word[i:]
+                    # if we can find word add to its left to make them to a palindrome
+                    if left == left[::-1] and right in rev_words and idx != rev_words[right]:
+                        res.append([rev_words[right], idx])
+                    # add to right
+                    if right == right[::-1] and left in rev_words and idx != rev_words[left]:
+                        res.append([idx, rev_words[left]])
+            else:
+                # for ''
+                for loc in palidStr:
+                    if loc != idx:
+                        res.append([idx, loc])
+        return res
+
+
+
+
 
 words=["a","","abc","aba"]
 res=Solution()
