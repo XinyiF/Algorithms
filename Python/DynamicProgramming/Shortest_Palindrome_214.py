5269750a3ab06f70c6b2e723def84b4cf911a95b
xfeng82 <xfeng82@wisc.edu>
2020-01-03 02:09:32 -0600

    make string s to be shortest palindrome by add string to its left side I need better and faster method

---
+++ Python/DynamicProgramming/Shortest_Palindrome_214.py
@@ -0,0 +1,58 @@
+#Given a string s, you are allowed to convert it to a palindrome by adding characters in front of it.
+# Find and return the shortest palindrome you can find by performing this transformation.
+
+class Solution(object):
+    def CenterExpand_neib(self, left, right, count, cur, s):
+        flag=0
+        while left>=0 and right<len(s):
+            if s[left]==s[right]:flag=1;left-=1;right+=1
+            else:break
+        if left==-1 or flag==0 and left==0:
+            cur=s
+            for i in range(right,len(s)):
+                cur=s[i]+cur
+            count[cur] = len(cur)
+
+
+    def CenterExpand_betw(self, left, right, count, cur, s):
+        while left>=0 and right<len(s):
+            if s[left]==s[right]:left-=1;right+=1
+            else:break
+        if left==-1:
+            cur=s
+            for i in range(right, len(s)):
+                cur = s[i] + cur
+            count[cur] = len(cur)
+
+    def shortestPalindrome(self, s):
+        count={}
+        cur=''
+        rev=s[::-1]
+        flag=0
+        if len(s)<=1:return s
+        index = 0
+        for i in range(int(len(s)/2)):
+            cur=cur+s[i]
+            if cur==rev[len(s)-(i+1)-(i+1):len(s)-(i+1)]:
+                flag=0
+                count[i]=index
+                index+=1
+            if cur==rev[len(s)-(i+1)-(i+1)-1:len(s)-(i+1)-1]:
+                flag=1
+                count[i] = index
+                index += 1
+        if not count:end=1
+        else:
+            mid=max(count,key=count.get)
+            if flag==0:end=(mid+1)*2
+            if flag==1:end=(mid+1)*2+1
+        cur=s
+        for i in range(end,len(s)):
+            cur=s[i]+cur
+        return cur
+
+
+str="aaaa"  #hgfe dcba
+res=Solution()
+s=res.shortestPalindrome(str)
+print(s)
