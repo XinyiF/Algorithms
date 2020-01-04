d08abdd9bc965944e1be5f77efe3c08ec677220e
xfeng82 <xfeng82@wisc.edu>
2020-01-03 22:42:01 -0600

    faster than BF, max could be used repeatedly

--- Python/Array/Sliding_Window_Maximum_239.py
+++ Python/Array/Sliding_Window_Maximum_239.py
@@ -1,6 +1,9 @@
 #Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right.
 # You can only see the k numbers in the window. Each time the sliding window moves right by one position.
 # Return the max sliding window.
+
+from collections import deque
+
 class Solution(object):
     def maxSlidingWindow(self, nums, k):
         if len(nums)==0:return
@@ -11,10 +14,31 @@ class Solution(object):
             right+=1
         return res
 
+    def maxSlidingWindow_dq(self, nums, k):
+        if len(nums) == 0: return
+        if k==1:return nums
+        left, right, res = 0, k-1, []
+        Max=max(nums[0:k])
+        index=nums[0:k].index(Max)
+        while right < len(nums):
+            if index >=left:
+                Max=max(Max,nums[right])
+            else:
+                Max=max(nums[left:right+1])
+            index = nums[left:right + 1].index(Max)
+            res.append(Max)
+            left += 1
+            right += 1
+        return res
 
 
-nums = [1,3,-1,-3,5,3,6,7]
-k = 3
+
+
+
+
+
+nums = [7,2,4]
+k = 2
 res=Solution()
-resu=res.maxSlidingWindow(nums,k)
+resu=res.maxSlidingWindow_dq(nums,k)
 print(resu)
