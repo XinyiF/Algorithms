f1f3e4cb3bf51eef1fed65e418ee679901c2070d
xfeng82 <xfeng82@wisc.edu>
2020-01-05 20:31:16 -0600

    traversal of binary tree, recursive and iterative

--- Python/Tree/test.py
+++ Python/Tree/test.py
@@ -1,11 +1,120 @@
 class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
+    def __init__(self, value):
+        self.val = value
         self.left = None
         self.right = None
 
-tree=TreeNode('A')
-tree.val=1
-tree.left=TreeNode('B')
-tree.left.val=2
-print(tree)
+class BinaryTree(object):
+    def __init__(self,root):
+        self.root=TreeNode(root)
+
+    def print_tree(self,traversal_type):
+        if traversal_type=="preorder":
+            return self.preorder_print(tree.root,"")
+        else:print("maybe later")
+
+
+    def preorder_print(self,start,res):
+        if start:
+            res.append(start.val)
+            res=self.preorder_print(start.left,res)
+            res= self.preorder_print(start.right, res)
+        return res
+
+    def inorder_print(self,start,res):
+        if start:
+            res = self.inorder_print(start.left, res)
+            res.append(start.val)
+            res = self.inorder_print(start.right, res)
+        return res
+
+    #def postorder_print(self,start,res):
+     #   if start:
+      #      res = self.inorder_print(start.left, res)
+      #      res = self.inorder_print(start.right, res)
+       #     res.append(start.val)
+       # return res
+
+
+    def inorderTraversal_recursive(self, root):
+        res=[]
+        def print_tree(root):
+            if not root:
+                return
+            print_tree(root.left)
+            res.append(root.val)
+            print_tree(root.right)
+        print_tree(root)
+        return res
+
+    def preorderTraversal(self, root):
+            res = []  # store result
+            stack = []  # helper stack
+            cur = root  # current node
+            while stack or cur:
+                while cur:
+                    res.append(cur.val)
+                    stack.append(cur)
+                    cur = cur.left
+                top = stack.pop()
+                cur = top.right
+            return res
+
+    def inorderTraversal(self, root):
+        res=[]
+        stack=[]
+        cur=root
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur=cur.left
+            top=stack.pop()
+            res.append(top.val)
+            cur=top.right
+        return res
+
+    def postorderTraversal(self, root):
+            res = []
+            stack = []
+            cur = root
+            while stack or cur:
+                while cur:
+                    res.append(cur.val)
+                    stack.append(cur)
+                    cur = cur.right
+                top = stack.pop()
+                cur = top.left
+            return res[::-1]
+
+
+
+
+#          1
+#        /   \
+#       2     3
+#      / \   / \
+#     4   5 6   7
+tree=BinaryTree(1)
+tree.root.left=TreeNode(2)
+tree.root.right=TreeNode(3)
+tree.root.left.left=TreeNode(4)
+tree.root.left.right=TreeNode(5)
+tree.root.right.left=TreeNode(6)
+tree.root.right.right=TreeNode(7)
+res=[]
+res=tree.preorder_print(tree.root,res)
+print(res)
+res=tree.preorderTraversal(tree.root)
+print(res)
+
+res=[]
+res=tree.inorder_print(tree.root,res)
+print(res)
+res=tree.inorderTraversal(tree.root)
+print(res)
+
+res=[]
+#res=tree.postorder_print(tree.root,res)
+#print(res)
+res=tree.postorderTraversal(tree.root)
+print(res)
