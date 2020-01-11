c014677fd62a79ab126bba2b478e6e0261bd59a7
xfeng82 <xfeng82@wisc.edu>
2020-01-11 17:39:07 -0600

    mirror of a tree, recursive and iterative

--- Python/Tree/Mirror_tree.py
+++ Python/Tree/Mirror_tree.py
@@ -6,13 +6,27 @@ class TreeNode(object):
         self.right = None
 
 class Solution:
-    def Mirror(self, root):
+    def Mirror_recursive(self, root):
         if not root: return root
         temp= root.left
         root.left=root.right
         root.right=temp
         self.Mirror(root.left)
         self.Mirror(root.right)
+
+    def Mirror(self,root):
+        if not root: return root
+        cur,stack=root,[]
+        while stack or cur:
+            while cur:
+                temp=cur.left
+                cur.left=cur.right
+                cur.right=temp
+                stack.append(cur)
+                cur=cur.left
+            top=stack.pop()
+            cur =top.right
+        return root
 
 
 
