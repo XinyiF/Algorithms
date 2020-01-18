ba245713e48c20ed3ef890344b2f6c99e21084a9
xfeng82 <xfeng82@wisc.edu>
2020-01-18 17:48:58 -0600

    integer to roman

--- Python/String/Integer_to_Roman_12.py
+++ Python/String/Integer_to_Roman_12.py
@@ -1,6 +1,6 @@
-#https://leetcode.com/problems/integer-to-roman/description/
 class Solution:
     def intToRoman(self, num: int) -> str:
+        dic={1:'I',5:'V',10:'X',50:'L',100:'C',500:'D',1000:'M'}
         ones_place=["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
         tens_place=["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
         hundreds_place=["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
