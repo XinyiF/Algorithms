6f7952c81f77c27357c6f0e29e31888113fd2e65
xfeng82 <xfeng82@wisc.edu>
2020-12-19 20:58:26 -0600

    review

--- Python/Array/test.py
+++ Python/Array/test.py
@@ -1,6 +1,39 @@
-def extraLongFactorials(n):
-    if n==1:return 1
-    return n*extraLongFactorials(n-1)
+def BinarySearch(sortedArr,target,HigherBound=False):
+    """
 
-n=4
-print(extraLongFactorials(n))
+    :param sortedArr:
+    :param target:
+    :param HigherBound:
+    :return:
+    """
+    low,high=0,len(sortedArr)-1
+    # 为了确认上下边界
+    # low high 会在最后选择时重合
+    while low <= high:
+        if HigherBound:
+            if sortedArr[high] == target:
+                return high
+            # 找上边界要向上取整
+            mid = low + (high - low+1) // 2
+            # low快速收缩
+            if sortedArr[mid] < target:
+                low = mid + 1
+            elif sortedArr[mid] > target:
+                high=mid-1
+            else:
+                low=mid
+        else:
+            if sortedArr[low] == target:
+                return low
+            mid = low + (high - low) // 2
+            if sortedArr[mid] > target:
+                high = mid-1
+            elif sortedArr[mid] < target:
+                low = mid+1
+            else:
+                high=mid
+    return -1
+
+
+list=[5,7,7,8,8,10]
+print(BinarySearch(list,8,True))
